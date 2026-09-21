03a542738014f564c2a65b7e0324ba20225d1c1c
kev <14802@holbertonstudents.com>
2026-09-21 08:59:29 +0200

    edit positive_or_negative.py

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -2,12 +2,11 @@
 
 number = __import__('random').randint(-10, 10)
 
-for number in range(-10, 10):
-    if number > 0:
-        print(number, "is positive")
+if number > 0:
+    print(number, "is positive")
 
-    elif number == 0:
-        print(number, "is zero")
+elif number == 0:
+    print(number, "is zero")
 
-    else:
-        print(number, "is negative")
+else:
+    print(number, "is negative")

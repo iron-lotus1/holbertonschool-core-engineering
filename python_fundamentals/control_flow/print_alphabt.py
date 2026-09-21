fb3effb651640a8997b812ca8757228b23e4f61e
kev <14802@holbertonstudents.com>
2026-09-21 10:00:04 +0200

    add print_alphabt.py

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-print("abcdfghijklmnoprstuvwxyz")
+text = ""
+for letter in "abcdefghijklmnopqrstuvwxyz":
+    if letter != "e" and letter != "q":
+        text += letter
+print(text)

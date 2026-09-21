4c40d3ff9dee9be8e4f677a395f9eb3f27bf147c
Kev <kevin.hulbert93@gmail.com>
2026-09-22 09:56:29 +1000

    update line of code

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python3
 
-text = "abcdefghijklmnopqrstuvwxyz"
-exclude = "eq"
-
-alpha = ''.join([char for char in text if char not in exclude])
-print(alpha)
+print("{}".format("".join([chr(i) for i in range(97, 123) if i not in (101, 113)])))

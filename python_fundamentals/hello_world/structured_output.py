ac5cea7e2ae5945e629b0c4a14c10c4f1119af81
Kev <kevin.hulbert93@gmail.com>
2026-09-21 15:28:45 +1000

    updated structured py file

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 
-Language: "Python"
-version = 3
-Pi = 3.14
-Computation = True
-
-
-print("Language:", Language)
-print("Version:", version)
-print("Pi approx:", Pi)
-print("Computation valid:", Computation)
+print("Language:", "Python")
+print("Version:", 3)
+print("Pi approx:", 3.14)
+print("Computation valid:", True)

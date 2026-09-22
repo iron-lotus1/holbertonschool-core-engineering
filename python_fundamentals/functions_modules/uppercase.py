f7fc43ee70a144e87f6aad55f8bf127cc4693355
Kev <kevin.hulbert93@gmail.com>
2026-09-22 22:34:25 +1000

    update uppercase.py

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 def uppercase(str):
-    result = ""
     for char in str:
         if 97 <= ord(char) <= 122:
-            result += chr(ord(char) - 32)
-        else:
-            result += char
-            print("{}".format(result))
+            char = (ord(char) - 32)
+            print("{}".format(char), end="")
+            print("")

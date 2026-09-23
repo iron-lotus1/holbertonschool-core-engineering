97d09dc4edcbb6fd2d74c787429470d76299e798
Kev <kevin.hulbert93@gmail.com>
2026-09-23 20:59:25 +1000

    add print_last_digit.py to the DIR

---
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
+
+if __name__ == "__main__":
+    print_last_digit(98)
+    print()
+    print_last_digit(0)
+    print()
+    print_last_digit(-1024)
+    print()

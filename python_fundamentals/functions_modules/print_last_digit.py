dd578fc6376b227f5fd8b86fc6010c1e76b69729
Kev <kevin.hulbert93@gmail.com>
2026-09-23 21:02:38 +1000

    updated print_last_digit.py

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+
 def print_last_digit(number):
     last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
+
 
 if __name__ == "__main__":
     print_last_digit(98)
